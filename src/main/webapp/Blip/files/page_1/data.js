8e15015c24033f4832b1d771050176ab53204bd9
Michael Stokes <mstokes2@us.ibm.com>
2017-02-28 12:01:38 -0600

    Added BLIP!

---
+++ src/main/webapp/Blip/files/page_1/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i],j,_(k,l,m,n,o,p,q,_(r,s,t,u,v,_(w,x,y,z),A,null,B,u,C,u,D,E,F,null,G,H,I,J,K,L,M,H),N,_(),O,_(),P,_(Q,[])),R,_(),S,_());}; 
+var b="url",c="page_1.html",d="generationDate",e=new Date(1488299935137.96),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="page",k="packageId",l="92afd231abe34215b29c0ff261c1128a",m="type",n="Axure:Page",o="name",p="Page 1",q="style",r="baseStyle",s="627587b6038d43cca051c114ac41ad32",t="pageAlignment",u="near",v="fill",w="fillType",x="solid",y="color",z=0xFFFFFFFF,A="image",B="imageHorizontalAlignment",C="imageVerticalAlignment",D="imageRepeat",E="auto",F="favicon",G="sketchFactor",H="0",I="colorStyle",J="appliedColor",K="fontName",L="Applied Font",M="borderWidth",N="adaptiveStyles",O="interactionMap",P="diagram",Q="objects",R="masters",S="objectPaths";
+return _creator();
+})());
